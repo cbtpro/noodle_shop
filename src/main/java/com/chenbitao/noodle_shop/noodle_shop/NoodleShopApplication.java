c6cd9f2dd2ad4237b02293be5806e1582c8d5f9f
chenbitao <cbtpro@gmail.com>
2025-08-16 15:56:00 +0800

    feat: init

---
+++ src/main/java/com/chenbitao/noodle_shop/noodle_shop/NoodleShopApplication.java
@@ -0,0 +1,13 @@
+package com.chenbitao.noodle_shop.noodle_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoodleShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoodleShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chenbitao/noodle_shop/noodle_shop/NoodleShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenbitao.noodle_shop.noodle_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoodleShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
