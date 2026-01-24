35b90fe506d93d55d1b223f09cbdef64b1d73d89
陈碧滔 <cbtpro@163.com>
2026-01-24 10:04:00 +0800

    refactor: 使用of 替代 fromType

--- src/main/java/com/chenbitao/noodle_shop/enums/GoodsType.java
+++ src/main/java/com/chenbitao/noodle_shop/enums/GoodsType.java
@@ -1,7 +1,6 @@
 package com.chenbitao.noodle_shop.enums;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -26,11 +25,18 @@ public enum GoodsType {
 
     @JsonCreator
     public static GoodsType fromType(String type) {
+        return of(type);
+    }
+
+    public static GoodsType of(String type) {
+        if (type == null) {
+            return null;
+        }
         for (GoodsType gt : values()) {
             if (gt.code.equalsIgnoreCase(type)) {
                 return gt;
             }
         }
-        throw new IllegalArgumentException("Unknown type: " + type);
+        throw new IllegalArgumentException("Unknown GoodsType code: " + type);
     }
 }
